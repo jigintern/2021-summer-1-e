1760f2a1cd7b5f85636241470e48be882273bfe4
ribnil <40230941+ribnil@users.noreply.github.com>
2021-08-17 10:21:54 +0900

    database impl

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import {Server} from "https://js.sabae.cc/Server.js";
 new Server(8001);
+
+export class User {
+	constructor() {
+		import { DB } from "https://deno.land/x/sqlite/mod.ts";
+		this.db = new DB("travel.db");
+		this.db.query(`
+			CREATE TABLE IF NOT EXISTS people (
+		        id INTEGER PRIMARY KEY AUTOINCREMENT,
+		        name TEXT
+		    )
+		`).then(() => {});
+	}
+}
