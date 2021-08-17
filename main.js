02660d74633e418fdfbb7cdad1927c07cc801606
ribnil <40230941+ribnil@users.noreply.github.com>
2021-08-17 10:44:11 +0900

    クラス編集

--- main.js
+++ main.js
@@ -4,12 +4,27 @@ new Server(8001);
 export class TravelDB {
 	constructor() {
 		import { DB } from "https://deno.land/x/sqlite/mod.ts";
-		this.db = new DB("travel.db");
-		this.db.query(`
-			CREATE TABLE IF NOT EXISTS people (
+		self.db = new DB("travel.db");
+		self.db.query(`
+			CREATE TABLE IF NOT EXISTS accounts (
 		        id INTEGER PRIMARY KEY AUTOINCREMENT,
-		        name TEXT
+		        username TEXT,
+		        email TEXT,
+		        comment TEXT
 		    )
-		`).then(() => {});
+		`).then(() => {
+			console.log("DB init.");
+		});
+	}
+	
+	static getDB() {
+		return self.db;
+	}
+	
+	static add(username, email, comment) {
+		const db = self.getDB();
+		db.query("INSERT INTO accounts (username, email, comment) VALUES (${username})").then(() => {
+			console.log("add record \"${username}\"");
+		})
 	}
 }
