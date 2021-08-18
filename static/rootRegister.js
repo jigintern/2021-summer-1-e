20d4329aba623226a4afafd2dc281611b5ab2d46
kijosei <87769295+kijosei@users.noreply.github.com>
2021-08-19 03:18:54 +0900

    変更

--- static/rootRegister.js
+++ static/rootRegister.js
@@ -1,30 +1,30 @@
-import L from 'https://code4sabae.github.io/leaflet-mjs/leaflet.mjs'
-            
-window.onload = async function() {
-    const data = [
-    { lat: 35.943560, lng: 136.188917, name: '鯖江駅' },
-    { lat: 35.944539, lng: 136.186222, name: 'Hana道場' },
-    { lat: 35.942795, lng: 136.198881, name: 'めがね会館' },
-    { lat: 35.949658, lng: 136.258142, name: 'JAPAN CRAFT HOUSE' },
-    ]
-
-    const map = L.map('mapid')
-    // set 国土地理院地図 https://maps.gsi.go.jp/development/ichiran.html
-    L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
-        attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>"',
-        maxZoom: 18,
-    }).addTo(map)
-
-    let iconlayer = L.layerGroup()
-    iconlayer.addTo(map)
-    const lls = []
-    for (const item of data) {
-    const ll = [ item.lat, item.lng ]
-    const marker = L.marker(ll, { title: item.name })
-    marker.bindPopup(item.name)
-    iconlayer.addLayer(marker)
-    lls.push(ll)
+function coloeSet(argObj){
+    if(argObj.value==""){
+        argObj.style.backgroundColor = "";
+    }else{
+        argObj.style.backgroundColor = "rgba(0, 0, 127, 0.20)";
     }
-    if (lls.length)
-    map.fitBounds(lls)
+}
+
+function colorReset(argObj){
+    argObj.style.backgroundColor = "";
+}
+function coloeSet(argObj){
+    if(argObj.value==""){
+        argObj.style.backgroundColor = "";
+    }else{
+        argObj.style.backgroundColor = "rgba(0, 0, 127, 0.20)";
+    }
+}
+
+function colorReset(argObj){
+    argObj.style.backgroundColor = "";
+}
+
+function textAreaHeightSet(argObj){
+  argObj.style.height = "10px";
+  var wSclollHeight = parseInt(argObj.scrollHeight);
+  var wLineH = parseInt(argObj.style.lineHeight.replace(/px/, ''));
+  if(wSclollHeight < (wLineH * 2)){wSclollHeight=(wLineH * 2);}
+  argObj.style.height = wSclollHeight + "px";
 }
