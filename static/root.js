e38ad9e060121fbf9b4d910d3a4b739088846042
kijosei <87769295+kijosei@users.noreply.github.com>
2021-08-18 15:47:11 +0900

    ルート編集しました

--- static/root.js
+++ static/root.js
@@ -5,10 +5,8 @@ function clickBtn1(){
     const p1 = document.getElementById("p1");
 
     if(p1.style.display=="block"){
-        // noneで非表示
         p1.style.display ="none";
     }else{
-        // blockで表示
         p1.style.display ="block";
     }
 }
@@ -19,9 +17,6 @@ function countUpB() {
 }
 
 function coloeSet(argObj){
-  // ==============================================
-  //	入力済み時の背景色設定
-  // ==============================================
      if(argObj.value==""){
          argObj.style.backgroundColor = "";
      }else{
@@ -33,9 +28,6 @@ function coloeSet(argObj){
      argObj.style.backgroundColor = "";
  }
  function coloeSet(argObj){
-  // ==============================================
-  //	入力済み時の背景色設定
-  // ==============================================
      if(argObj.value==""){
          argObj.style.backgroundColor = "";
      }else{
@@ -44,23 +36,13 @@ function coloeSet(argObj){
  }
  
  function colorReset(argObj){
-  // ==============================================
-  //	フォーカス時の背景色リセット
-  // ==============================================
      argObj.style.backgroundColor = "";
  }
  
  function textAreaHeightSet(argObj){
-  // ==============================================
-  //	フォーカス時の背景色リセット
-  // ==============================================
-   // 一旦テキストエリアを小さくしてスクロールバー（縦の長さを取得）
    argObj.style.height = "10px";
    var wSclollHeight = parseInt(argObj.scrollHeight);
-   // 1行の長さを取得する
    var wLineH = parseInt(argObj.style.lineHeight.replace(/px/, ''));
-   // 最低2行の表示エリアにする
    if(wSclollHeight < (wLineH * 2)){wSclollHeight=(wLineH * 2);}
-   // テキストエリアの高さを設定する
    argObj.style.height = wSclollHeight + "px";
  }
