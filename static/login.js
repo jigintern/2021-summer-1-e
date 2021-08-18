622dac75b90595cf6617181cbc5f6b51c60f8364
nakabayashi0703 <77555225+nakabayashi0703@users.noreply.github.com>
2021-08-18 17:05:17 +0900

    123

--- static/login.js
+++ static/login.js
@@ -2,3 +2,8 @@ const getdata = () =>{
 let text = document.getElementById("text");
 let password = document.getElementById("password");
 }
+function save() {
+    var mydata = document.getElementById("mydata_in").value;
+    console.log(`mydata_in = ${mydata_in}`);
+    localStorage.setItem('mydata', mydata);
+  };
