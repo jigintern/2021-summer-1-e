cbc560a3503ef95b66372089f4fbafaca60b444c
nakabayashi0703 <77555225+nakabayashi0703@users.noreply.github.com>
2021-08-18 17:16:19 +0900

    aaa

--- static/login.js
+++ static/login.js
@@ -1,3 +1,5 @@
+import {fetchJSON} from "https://js.sabae.cc/fetchJSON.js";
+
 const getdata = () =>{
 let text = document.getElementById("text");
 let password = document.getElementById("password");
@@ -7,3 +9,27 @@ function save() {
     console.log(`mydata_in = ${mydata_in}`);
     localStorage.setItem('mydata', mydata);
   };
+
+async function getAccount(key, password) {
+  const result = await fetchJSON("api/account/find", {
+    key: key,
+    password: password
+  });
+
+  return result;
+}
+
+
+
+
+
+let submit = document.getElementById('submit');
+        
+        
+        submit.onclick = async () => {
+            console.log("click!!")
+            const existResult = await fetchJSON("api/account/exist", {
+                key: key,
+                password: password
+            });
+          }
