9fe6c79c14d565d11ef8a10a6d83fca9e886895c
shoya-kouno <86906571+shoya-kouno@users.noreply.github.com>
2021-08-19 23:53:19 +0900

    updata

--- static/rootTransition.js
+++ static/rootTransition.js
@@ -2,4 +2,4 @@
     const t1 = document.getElementById("inp_body").value;
     window.location.href = 'root.html';
   }
-  document.getElementById("span1").textContent = t1;
+  //document.getElementById("span1").textContent = t1;
